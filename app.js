9cbd9cf32738f8159c13d1864d8e7911352a342b
Tim Keller <timothy.m.keller@gmail.com>
2021-01-13 21:16:07 -0800

    Added imports for all questions, and functions to add new employees.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const { managerQuestions, engineerQuestions, internQuestions, nextStep } = require("./lib/questions")
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -9,54 +10,81 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+console.log( managerQuestions );
 
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
-const managerQuestions = [
-    {
-        type: 'input',
-        message: 'Manager name:',
-        name: 'name'
+const employeeArray = [];
+
+inquirer.prompt( managerQuestions ).then( ( response, err ) => {
+    if ( response ) {
+        const manager = new Manager( response.name, response.id, response.email, response.officeNumber );
+        employeeArray.push( manager );
+        console.log( employeeArray );
+
+    } else {
+        console.log( err );
     }
-]
+});
 
-const greg = new Manager( "Greg", 1, "greg@greg.com", 12 );
-const alice = new Engineer( "Alice", 2, "alice@alice.com", "alicecode" )
-const sam = new Intern( "Sam", 3, "sam@sam.com", "UW" );
+function addNew() {
+    inquirer.prompt( nextStep ).then( ( response, err ) => {
+        if ( response ) {
+            switch ( response.next ) {
+                case "Add an engineer":
+                    break;
+                case "Add an intern":
+                    break;
+                default:
+                    break;
+            }
+        } else {
+            console.log( err );
+        }
+    });
+}
 
-const empArr = [ greg, alice, sam ];
+function addEngineer() {
+    inquirer.prompt( engineerQuestions ).then( ( response, err ) => {
+        if ( response ) {
+            console.log( response );
+            const engineer = new Engineer( response.name, response.id, response.email, response.github );
+            employeeArray.push( engineer );
+        } else {
+            console.log( err );
+        }
+    });
+}
 
-const rendies = render( empArr );
-fs.appendFile( './output/team.html', rendies, ( err ) => {
-    err ? console.log( err ) : console.log( "It worked!" );
-})
-
-// inquirer.prompt( managerQuestions ).then( response => {
-//     var string = JSON.stringify( response );
-//     if ( response ) {
-//         fs.readFile( './templates/manager.html', 'utf8', ( error, data ) => {
-//             if ( error ) {
-//                 console.log( error );
-//             } else {
-//                 fs.appendFile( './output/team.html', data, ( err ) => {
-//                     err ? console.log( err ) : console.log( "It worked!" );
-//                 })
-//             }
-//         })
-//     }
-// });
+function addIntern() {
+    inquirer.prompt( internQuestions ).then( ( response, err ) => {
+        if ( response ) {
+            console.log( response );
+            const intern = new Intern( response.name, response.id, response.email, response.school );
+            employeeArray.push( intern );
+        } else {
+            console.log( err );
+        }
+    });
+}
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
+
+const rendies = render( employeeArray );
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
+
+// fs.writeFile( './output/team.html', rendies, ( err ) => {
+//     err ? console.log( err ) : console.log( "It worked!" );
+// })
 
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
